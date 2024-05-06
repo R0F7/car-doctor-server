23f75bf9c3184a88b50db4b20b91c47063a3b4c5
R0F7 <wwwrafikhan075@gmail.com>
2024-05-06 16:48:47 +0600

    create verify middleware, set token cookie , and use token

---
+++ __jwt__token.js
@@ -0,0 +1,12 @@
+/**
+ * ---------------------
+ * MAKE API SECURE
+ * ---------------------
+ * The person who should have 
+ * 
+ * concept:
+ * 1.assign two tokens for each person (access token, refresh token)
+ * 2.access token contains: user identification (email, role etc.). valid for shorter duration 
+ * 3.refresh token is used: to recreate an access token that was expired.
+ * 4.if refresh is invalid then logout the user
+ */
--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000;
 
 //middleware
 app.use(cors({
-    origin:['http://localhost:5173'],
+    origin: ['http://localhost:5173'],
     credentials: true,
 }));
 app.use(express.json());
@@ -34,6 +34,30 @@ const logger = async (req, res, next) => {
     next();
 }
 
+//create middleware for verify token
+const verifyToken = async (req, res, next) => {
+    const token = req.cookies?.token;
+    console.log('value of token in middleware', token);
+
+    if (!token) {
+        return res.status(401).send({ message: 'not authorized' })
+    }
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        //err
+        if (err) {
+            console.log(err);
+            return res.status(401).send({ message: 'unauthorized' })
+        }
+
+        //if token is valid then it would be decoded
+        console.log('value in the token ', decoded);
+        req.user = decoded;
+        next();
+
+    })
+}
+
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
@@ -43,7 +67,7 @@ async function run() {
         const bookingCollection = client.db('carDoctor').collection('bookings');
 
         //auth related api
-        app.post('/jwt', async (req, res) => {
+        app.post('/jwt', logger, async (req, res) => {
             const user = req.body;
             console.log(user);
             const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
@@ -57,7 +81,7 @@ async function run() {
         })
 
         //services related api
-        app.get('/services', async (req, res) => {
+        app.get('/services', logger, async (req, res) => {
             const cursor = serviceCollection.find();
             const result = await cursor.toArray();
             res.send(result)
@@ -74,10 +98,14 @@ async function run() {
         })
 
         //bookings
-        app.get('/booking', async (req, res) => {
+        app.get('/booking', logger, verifyToken, async (req, res) => {
             // console.log(req.query.email);
+            // console.log('tok tok token', req.cookies);
 
-            console.log('tok tok token', req.cookies);
+            console.log('user in the valid token', req.user);
+            if (req.query.email !== req.user.email) {
+                return res.status(401).send({ message: 'forbidden access' })
+            }
 
             let query = {};
             if (req.query.email) {
